0a8a4952bf369aa1bcb6d73e86538a4e4c9328f8
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-08-28 17:36:55 +0200

    Add tests for AppVeyor and TravisCI

---
+++ src/Serilog.Exceptions.Tests/Program.cs
@@ -0,0 +1,47 @@
+﻿using Serilog.Exceptions.Destructurers;
+
+namespace Serilog.Exceptions.Tests
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestExceptionDestructurer();
+            TestArgumentExceptionDestructurer();
+            TestArgumentOutOfRangeExceptionDestructurer();
+            TestAggregateExceptionDestructurer();
+            TestReflectionTypeLoadExceptionDestructurer();
+            TestSqlExceptionDestructurer();
+        }
+
+        private static ExceptionDestructurer TestExceptionDestructurer()
+        {
+            return new ExceptionDestructurer();
+        }
+
+        private static ExceptionDestructurer TestArgumentExceptionDestructurer()
+        {
+            return new ArgumentExceptionDestructurer();
+        }
+
+        private static ExceptionDestructurer TestArgumentOutOfRangeExceptionDestructurer()
+        {
+            return new ArgumentOutOfRangeExceptionDestructurer();
+        }
+
+        private static ExceptionDestructurer TestAggregateExceptionDestructurer()
+        {
+            return new AggregateExceptionDestructurer();
+        }
+
+        private static ExceptionDestructurer TestReflectionTypeLoadExceptionDestructurer()
+        {
+            return new ReflectionTypeLoadExceptionDestructurer();
+        }
+
+        private static ExceptionDestructurer TestSqlExceptionDestructurer()
+        {
+            return new SqlExceptionDestructurer();
+        }
+    }
+}
